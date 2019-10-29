cc9c67a4fe4164b975725716c9b638ff4d1359cd
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-10-28 22:17:30 -0300

    Cambios hechos

---
+++ project/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Crear 4 funciones home, about, portfolio & contact
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
+
