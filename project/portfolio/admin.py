e4c3a6d6cad4384999ad246f2ddad599858a2d75
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-11-17 16:24:18 -0300

    Changes

--- project/portfolio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Project
-
-class ProjectAdmin(admin.ModelAdmin):
-    readonly_fields = ('created','updated') #
-
-admin.site.register(Project, ProjectAdmin)
